51898c711b1e3d110a6c1bc4be54e0797cb87707
Felippe Vaz <felippe.vz@gmail.com>
2026-03-14 16:48:43 -0300

    Initial commit

---
+++ src/main/java/dev/felippevaz/vzp_company_service/VzpCompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.felippevaz.vzp_company_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VzpCompanyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VzpCompanyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/felippevaz/vzp_company_service/VzpCompanyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.felippevaz.vzp_company_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VzpCompanyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
