e42dbcb7c0f5e53ed6e9828e7267ee59e41c4ca2
Felippe Vaz <felippe.vz@gmail.com>
2026-03-17 17:49:56 -0300

    fix: add annotations Lombok, create prePersist datas

--- src/main/java/dev/felippevaz/vzp_company_service/modules/company/domain/Company.java
+++ src/main/java/dev/felippevaz/vzp_company_service/modules/company/domain/Company.java
@@ -3,11 +3,20 @@ package dev.felippevaz.vzp_company_service.modules.company.domain;
 import dev.felippevaz.vzp_company_service.common.domain.Address;
 import dev.felippevaz.vzp_company_service.modules.employee.domain.Employee;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Company {
 
     @Id
@@ -21,14 +30,19 @@ public class Company {
     private Address address;
 
     private List<String> emails;
-    private List<String> phoneNumbers;
+    private List<String> phones;
 
-    @OneToMany(mappedBy = "enterprise")
+    @OneToMany(mappedBy = "company")
     private List<Employee> employees;
 
     private LocalDate foundation;
-    private LocalDate startedServiceAt;
+    private LocalDateTime startedServiceAt;
 
     private ServiceStatus status;
 
+    @PrePersist
+    private void prePersist() {
+        this.startedServiceAt = LocalDateTime.now();
+        this.status = ServiceStatus.ACTIVE;
+    }
 }
