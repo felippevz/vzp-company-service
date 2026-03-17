96bf3dffa40cb02067df593b6e28ab5ea19bf5ed
Felippe Vaz <felippe.vz@gmail.com>
2026-03-17 17:53:43 -0300

    feat: implement Request/Response DTO in Service

--- src/main/java/dev/felippevaz/vzp_company_service/modules/company/service/CompanyService.java
+++ src/main/java/dev/felippevaz/vzp_company_service/modules/company/service/CompanyService.java
@@ -1,9 +1,14 @@
 package dev.felippevaz.vzp_company_service.modules.company.service;
 
+import dev.felippevaz.vzp_company_service.exceptions.ServiceException;
 import dev.felippevaz.vzp_company_service.modules.company.domain.Company;
+import dev.felippevaz.vzp_company_service.modules.company.dto.request.CompanyRequestDTO;
+import dev.felippevaz.vzp_company_service.modules.company.dto.response.CompanyResponseDTO;
+import dev.felippevaz.vzp_company_service.modules.company.mapper.CompanyMapper;
 import dev.felippevaz.vzp_company_service.modules.company.repository.CompanyRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,29 +17,34 @@ import java.util.List;
 public class CompanyService {
 
     private final CompanyRepository repository;
+    private final CompanyMapper companyMapper;
 
-    public CompanyService(CompanyRepository repository) {
+    public CompanyService(CompanyRepository repository, CompanyMapper companyMapper) {
         this.repository = repository;
+        this.companyMapper = companyMapper;
     }
 
-    public List<Company> findAll() {
-        return this.repository.findAll();
+    public List<CompanyResponseDTO> findAll() {
+        return this.companyMapper.toResponseDTOList(this.repository.findAll());
     }
 
-    public Company create(Company company) {
-        return this.repository.save(company);
+    public CompanyResponseDTO create(CompanyRequestDTO companyRequestDTO) {
+        return this.companyMapper.toResponseDTO(
+                this.repository.save(this.companyMapper.toEntity(companyRequestDTO))
+        );
     }
 
-    public Company read(Long id) {
-        return getCompany(id);
+    public CompanyResponseDTO read(Long id) {
+        return this.companyMapper.toResponseDTO(getCompany(id));
     }
 
-    public Company update(Long id, Company updatedCompany) {
+    public CompanyResponseDTO update(Long id, CompanyRequestDTO updatedCompany) {
 
-        Company Company = getCompany(id);
-        BeanUtils.copyProperties(updatedCompany, Company);
+        Company company = getCompany(id);
 
-        return Company;
+        this.companyMapper.updateEntityFromDTO(updatedCompany, company);
+
+        return this.companyMapper.toResponseDTO(this.repository.save(company));
     }
 
     public void delete(Long id) {
@@ -42,10 +52,7 @@ public class CompanyService {
     }
 
     private Company getCompany(Long id) {
-
-        //todo: treat error later
-
-        return this.repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Company not found!"));
+        return this.repository.findById(id).orElseThrow(() -> new ServiceException("company.not.found", HttpStatus.NOT_FOUND, id));
     }
 }
 
