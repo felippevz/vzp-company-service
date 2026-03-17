aaa5e3314a4c90afb6fe6d6f7198c93df393f398
Felippe Vaz <felippe.vz@gmail.com>
2026-03-17 17:51:10 -0300

    feat: add mapping methods between Entity and DTO

--- src/main/java/dev/felippevaz/vzp_company_service/modules/company/mapper/CompanyMapper.java
+++ src/main/java/dev/felippevaz/vzp_company_service/modules/company/mapper/CompanyMapper.java
@@ -1,9 +1,29 @@
 package dev.felippevaz.vzp_company_service.modules.company.mapper;
 
-import org.mapstruct.Mapper;
+import dev.felippevaz.vzp_company_service.modules.company.domain.Company;
+import dev.felippevaz.vzp_company_service.modules.company.dto.request.CompanyRequestDTO;
+import dev.felippevaz.vzp_company_service.modules.company.dto.response.CompanyResponseDTO;
+import org.mapstruct.*;
 
-@Mapper
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface CompanyMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "startedServiceAt", ignore = true)
+    @Mapping(target = "employees", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    Company toEntity(CompanyRequestDTO companyRequestDTO);
 
+    CompanyResponseDTO toResponseDTO(Company company);
+
+    List<CompanyResponseDTO> toResponseDTOList(List<Company> companies);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "startedServiceAt", ignore = true)
+    @Mapping(target = "employees", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDTO(CompanyRequestDTO dto, @MappingTarget Company entity);
 }
